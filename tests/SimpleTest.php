af5cd505f43dcb9fed64cc1171a32fd7d663152c
sidibos <sidibemousse@hotmail.com>
2019-04-28 13:49:10 +0100

    Add first test case

---
+++ tests/SimpleTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sidibos
+ * Date: 28/04/2019
+ * Time: 13:46
+ */
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testShouldBesuccessfull()
+    {
+        $this->assertTrue(true);
+    }
+}
